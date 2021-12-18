f6b5c5f9bfa7a18ff296f36137414ced500eb286
Simon L. Lange <simonllange@gmail.com>
2021-12-18 23:31:29 +0100

    hejsa

--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,130 @@ import Paragraph from "../components/paragraph";
 import tools from "../data/data.json";
 import programs from "../data/dataprogrammer.json";
 import libraries from "../data/datalibraries.json";
-import ParticlesComponent from "../components/particles";
+import Particles from "react-tsparticles";
+import { useState, useEffect } from "react";
 
 const Page = () => {
+  const [showParticles, setShowParticles] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowParticles(true);
+    }, 500);
+  }, []);
   return (
     <Container maxW="container.md">
-      <ParticlesComponent />
+      {showParticles} && (
+      <Particles
+        style={{ position: "fixed" }}
+        options={{
+          background: {
+            color: {
+              // eslint-disable-next-line react-hooks/rules-of-hooks
+              value: useColorModeValue("#ffffff", "#25252b"),
+            },
+            image: "url('')",
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "20%",
+          },
+          fullScreen: {
+            zIndex: -999,
+          },
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "repulse",
+              },
+              onHover: {
+                enable: false,
+                mode: "bubble",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                opacity: 0,
+                size: 0,
+              },
+              grab: {
+                distance: 400,
+              },
+              repulse: {
+                distance: 400,
+              },
+            },
+          },
+          particles: {
+            color: {
+              // eslint-disable-next-line react-hooks/rules-of-hooks
+              value: useColorModeValue("#D6BCFA", "#ffffff"),
+            },
+            links: {
+              color: {
+                value: "#ffffff",
+              },
+              distance: 150,
+              opacity: 0.4,
+            },
+            move: {
+              attract: {
+                rotate: {
+                  x: 600,
+                  y: 600,
+                },
+              },
+              enable: true,
+              path: {},
+              outModes: {
+                bottom: "out",
+                left: "out",
+                right: "out",
+                top: "out",
+              },
+              random: true,
+              speed: 1,
+              spin: {},
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 160,
+            },
+            opacity: {
+              random: {
+                enable: true,
+              },
+              value: {
+                min: 0,
+                max: 1,
+              },
+              animation: {
+                enable: true,
+                speed: 1,
+                minimumValue: 0,
+              },
+            },
+            size: {
+              random: {
+                enable: true,
+              },
+              value: {
+                min: 1,
+                max: 3,
+              },
+              animation: {
+                speed: 4,
+                minimumValue: 0.3,
+              },
+            },
+          },
+        }}
+      />
+      )
       <Section>
         <Box my={12} align="center">
           <p>
