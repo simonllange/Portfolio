6a87845941820186183e11811abd777dd3e09dd0
Simon L. Lange <simonllange@gmail.com>
2021-12-19 00:28:50 +0100

    bugfixing

--- components/section.jsx
+++ components/section.jsx
@@ -7,6 +7,14 @@ const StyledDiv = chakra(motion.div, {
   },
 });
 
-const Section = ({ children = 0 }) => <StyledDiv>{children}</StyledDiv>;
+const Section = ({ children, delay = 0 }) => (
+  <StyledDiv
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay }}
+  >
+    {children}
+  </StyledDiv>
+);
 
 export default Section;
