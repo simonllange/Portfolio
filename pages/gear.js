5fd1d310070853f5e53730e2fe04c5b925c2856a
Simon L. Lange <simonllange@gmail.com>
2021-12-19 14:54:20 +0100

    fixes and improvements

--- pages/gear.js
+++ pages/gear.js
@@ -17,73 +17,80 @@ import Section from "../components/section";
 import Link from "next/link";
 import gearData from "../data/datagear.json";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-
+import Layout from "../components/layout/article";
 const GearPage = () => {
   return (
-    <Container maxW="container.md">
-      <Section>
-        <Heading mt={20} variant="section-title">
-          Gear.
-        </Heading>
-        <Text>
-          Her er en liste over det hardware jeg bruger til hverdag.{" "}
-          <strong>Links er ikke affiliated.</strong> For at se hvilke
-          redskaber/sprog jeg bruger, gå tilbage til{" "}
-          <Link href="/#toolsPage" passHref>
-            <Box
-              display="inline-block"
-              color={useColorModeValue("purple.400", "red.200")}
-              cursor="pointer"
-            >
-              forsiden
-            </Box>
-          </Link>
-        </Text>
-      </Section>
-      <Section>
-        {gearData &&
-          gearData.gearData.map(({ title, image, description, url }) => (
-            <>
-              <LinkBox role="group" as="article" my={10}>
-                <Stack
-                  direction={{ base: "column", md: "row" }}
-                  p={3}
-                  spacing={6}
-                  justifyContent={{ base: "center", md: "flex-start" }}
-                  alignItems="center"
-                  w="full"
-                  my={5}
-                >
-                  <AspectRatio ratio={1} w="80px">
-                    <Image alt={title} src={image} layout="fill" />
-                  </AspectRatio>
-                  <VStack spacing={0} alignItems="flex-start" flex={1} w="full">
-                    <LinkOverlay href={url} isExternal>
-                      <Heading pb={2} size="md">
-                        {title}
-                      </Heading>
-                    </LinkOverlay>
-                    <Text fontSize="smd">{description}</Text>
-                  </VStack>
-                  <HStack w={12} justifyContent="flex-start">
-                    <Icon
-                      boxSize={6}
-                      as={ExternalLinkIcon}
-                      // eslint-disable-next-line react-hooks/rules-of-hooks
-                      color={useColorModeValue("purple.300", "red.200")}
-                      opacity={0}
-                      _groupHover={{ ml: 6, opacity: 1 }}
-                      transitionProperty="all"
-                      transitionDuration="slow"
-                      transitionTimingFunction="ease-out"
-                    />
-                  </HStack>
-                </Stack>
-              </LinkBox>
-            </>
-          ))}
-      </Section>
-    </Container>
+    <Layout>
+      <Container maxW="container.md">
+        <Section>
+          <Heading mt={20} variant="section-title">
+            Gear.
+          </Heading>
+          <Text>
+            Her er en liste over det hardware jeg bruger til hverdag.{" "}
+            <strong>Links er ikke affiliated.</strong> For at se hvilke
+            redskaber/sprog jeg bruger, gå tilbage til{" "}
+            <Link href="/#toolsPage" passHref>
+              <Box
+                display="inline-block"
+                color={useColorModeValue("purple.400", "red.200")}
+                cursor="pointer"
+              >
+                forsiden
+              </Box>
+            </Link>
+          </Text>
+        </Section>
+        <Section>
+          {gearData &&
+            gearData.gearData.map(({ title, image, description, url }) => (
+              <>
+                <LinkBox role="group" as="article" my={10}>
+                  <Stack
+                    direction={{ base: "column", md: "row" }}
+                    p={3}
+                    spacing={6}
+                    justifyContent={{ base: "center", md: "flex-start" }}
+                    alignItems="center"
+                    w="full"
+                    my={5}
+                  >
+                    <AspectRatio ratio={1} w="80px">
+                      <Image alt={title} src={image} layout="fill" />
+                    </AspectRatio>
+                    <VStack
+                      spacing={0}
+                      alignItems="flex-start"
+                      flex={1}
+                      w="full"
+                    >
+                      <LinkOverlay href={url} isExternal>
+                        <Heading pb={2} size="md">
+                          {title}
+                        </Heading>
+                      </LinkOverlay>
+                      <Text fontSize="smd">{description}</Text>
+                    </VStack>
+                    <HStack w={12} justifyContent="flex-start">
+                      <Icon
+                        boxSize={6}
+                        as={ExternalLinkIcon}
+                        // eslint-disable-next-line react-hooks/rules-of-hooks
+                        color={useColorModeValue("purple.300", "red.200")}
+                        opacity={0}
+                        _groupHover={{ ml: 6, opacity: 1 }}
+                        transitionProperty="all"
+                        transitionDuration="slow"
+                        transitionTimingFunction="ease-out"
+                      />
+                    </HStack>
+                  </Stack>
+                </LinkBox>
+              </>
+            ))}
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -15,89 +15,92 @@ import {
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import Section from "../components/section";
 import portfolioData from "../data/dataportfolio.json";
+import Layout from "../components/layout/article";
 
 const Portfolio = () => {
   return (
-    <Container maxW="container.md">
-      <Section>
-        <Heading mt={20} variant="section-title">
-          Min portfolio.
-        </Heading>
-        <Text>Her kan du se, hvilke projekter og cases jeg har lavet. </Text>
-        <Text>
-          <strong>
-            <i>More to come...</i>
-          </strong>
-        </Text>
-      </Section>
-      <Section>
-        {portfolioData &&
-          portfolioData.portfolioData.map(
-            ({ title, image, description, url }) => (
-              <>
-                <LinkBox
-                  // eslint-disable-next-line react-hooks/rules-of-hooks
-                  bg={useColorModeValue("gray.200", "gray.700")}
-                  role="group"
-                  as="article"
-                  my={10}
-                  borderRadius={10}
-                  transitionProperty="transform"
-                  transitionDuration="slow"
-                  transitionTimingFunction="ease-out"
-                  _hover={{ transform: "scale(1.025, 1.025)" }}
-                >
-                  <Stack
-                    direction={{ base: "column", md: "row" }}
-                    p={6}
-                    spacing={6}
-                    justifyContent={{ base: "center", md: "flex-start" }}
-                    alignItems="center"
-                    w="full"
-                    my={5}
+    <Layout>
+      <Container maxW="container.md">
+        <Section>
+          <Heading mt={20} variant="section-title">
+            Min portfolio.
+          </Heading>
+          <Text>Her kan du se, hvilke projekter og cases jeg har lavet. </Text>
+          <Text>
+            <strong>
+              <i>More to come...</i>
+            </strong>
+          </Text>
+        </Section>
+        <Section>
+          {portfolioData &&
+            portfolioData.portfolioData.map(
+              ({ title, image, description, url }) => (
+                <>
+                  <LinkBox
+                    // eslint-disable-next-line react-hooks/rules-of-hooks
+                    bg={useColorModeValue("gray.200", "gray.700")}
+                    role="group"
+                    as="article"
+                    my={10}
+                    borderRadius={10}
+                    transitionProperty="transform"
+                    transitionDuration="slow"
+                    transitionTimingFunction="ease-out"
+                    _hover={{ transform: "scale(1.025, 1.025)" }}
                   >
-                    <AspectRatio ratio={1} w="80px">
-                      <Image alt={title} src={image} layout="fill" />
-                    </AspectRatio>
-                    <VStack
-                      spacing={0}
-                      alignItems="flex-start"
-                      flex={1}
+                    <Stack
+                      direction={{ base: "column", md: "row" }}
+                      p={6}
+                      spacing={6}
+                      justifyContent={{ base: "center", md: "flex-start" }}
+                      alignItems="center"
                       w="full"
+                      my={5}
                     >
-                      <LinkOverlay
-                        display="flex"
-                        justifyContent="space-between"
-                        href={url}
+                      <AspectRatio ratio={1} w="80px">
+                        <Image alt={title} src={image} layout="fill" />
+                      </AspectRatio>
+                      <VStack
+                        spacing={0}
+                        alignItems="flex-start"
+                        flex={1}
                         w="full"
-                        isExternal
                       >
-                        <Heading pb={2} size="sm">
-                          {title}
-                        </Heading>
-                      </LinkOverlay>
-                      <Text fontSize="sm">{description}</Text>
-                    </VStack>
-                    <HStack w={12} justifyContent="flex-start">
-                      <Icon
-                        boxSize={5}
-                        as={ExternalLinkIcon}
-                        // eslint-disable-next-line react-hooks/rules-of-hooks
-                        color={useColorModeValue("purple.300", "red.200")}
-                        opacity={0}
-                        _groupHover={{ opacity: 1 }}
-                        transitionProperty="all"
-                        transitionDuration="slow"
-                        transitionTimingFunction="ease-out"
-                      />
-                    </HStack>
-                  </Stack>
-                </LinkBox>
-              </>
-            )
-          )}
-      </Section>
-    </Container>
+                        <LinkOverlay
+                          display="flex"
+                          justifyContent="space-between"
+                          href={url}
+                          w="full"
+                          isExternal
+                        >
+                          <Heading pb={2} size="sm">
+                            {title}
+                          </Heading>
+                        </LinkOverlay>
+                        <Text fontSize="sm">{description}</Text>
+                      </VStack>
+                      <HStack w={12} justifyContent="flex-start">
+                        <Icon
+                          boxSize={5}
+                          as={ExternalLinkIcon}
+                          // eslint-disable-next-line react-hooks/rules-of-hooks
+                          color={useColorModeValue("purple.300", "red.200")}
+                          opacity={0}
+                          _groupHover={{ opacity: 1 }}
+                          transitionProperty="all"
+                          transitionDuration="slow"
+                          transitionTimingFunction="ease-out"
+                        />
+                      </HStack>
+                    </Stack>
+                  </LinkBox>
+                </>
+              )
+            )}
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 
